b1d02f13e68a2916330a3f5228500ef42ef8562d
Kiril Traykov <kiril@newschool.edu>
2017-10-17 15:09:09 -0400

    Sending work

---
+++ jsClass/main.js
@@ -0,0 +1,34 @@
+
+
+var titleNames = ["Another Air", 
+"I assume a comfortable position", 
+"Word Web",
+"Day and Night",
+"Early August",
+"Smog",
+"Evening",
+"The Wooden Spoon",
+"Portrait",
+"Daybreak",
+"Sugar"
+]
+
+var title = choice(titleNames);
+var color = choice(titleColors);
+
+function choice(arr){
+  return arr[Math.floor(Math.random() * arr.length)]
+}
+
+var titleColors = [
+"#d6641b",
+"#516735",
+"#c4776e",
+"#bfddde",
+"#faf691",
+"#781f1f",
+"#6c0d6d",
+"#007dc6",
+"#ffc220",
+"#004c91",
+"#d6a36e"]
