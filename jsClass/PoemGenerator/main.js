f019159be8771d6a7d4e67f8a8254325575fa246
Kiril Traykov <kiril@newschool.edu>
2017-10-20 11:58:07 -0400

    Uploading Final Generator Assignment

--- jsClass/PoemGenerator/main.js
+++ jsClass/PoemGenerator/main.js
@@ -18,7 +18,10 @@ var titleColors = [
 "#0c8b87"]
 
         var count = poemDetails.length;
-        var footnote = "There are currently " + count + " poems to read from. Each is displayed by random selection."
+        var footnote1 = "There are currently " + count + " poems to read from."
+        var footnote2 = "Each is displayed by random selection."
+        var opensource = "This is an open-source project. If you wish to contribute and submit a published poem, please send me an "
+        var email = {permalink_url : "whatever.com", title : "email"};
 
     $(document).ready(function(){
 		$('.button').on('click', function(){
@@ -31,7 +34,10 @@ var titleColors = [
 		$('#body').html(poem[1]);
 		$('#author').text("Author: " + poem[2]);
 		$('#translator').text("Translator: " + poem[3]);
-		$('.footnote').text(footnote);
+		$('#footnote1').text(footnote1);
+		$('#footnote2').text(footnote2);
+		$('#opensource').text(opensource).css('border', '2px solid #708238');
+		$("<a></a>", {href : "mailto:kiril_traykov@yahoo.com", html : email.title}).appendTo("#opensource");
 
 		})
 
