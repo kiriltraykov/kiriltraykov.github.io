c4391882ad7b95396fd0373b343ae5c97d8e33d0
Kiril Traykov <kiril_traykov@yahoo.com>
2018-09-26 00:18:22 -0400

    Add files via upload

---
+++ 05CarColors/main.js
@@ -0,0 +1,32 @@
+
+   $(document).ready(function(){
+
+      $('#button0').hide();
+      $('#sketchholder1').hide();
+
+      $('body').on('click', function(){
+
+          $('body').on('click', function(){
+
+                $('#button0').show(); 
+
+                $('#button0').on('click', function(){
+
+                $('.highway').remove();
+                $('#button0').remove();
+                $('h4').remove();
+                $('#dendogramholder').remove();
+                $('#footnote2').remove();
+                $('#sketchholder1').show();
+
+                 
+
+        });
+         
+        });
+
+        });
+
+        });
+
+  
