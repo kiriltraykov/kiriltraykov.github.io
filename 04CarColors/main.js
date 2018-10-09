d5ed772a7e6c4106d9573aed8659ee73aa60d612
Kiril Traykov <kiril_traykov@yahoo.com>
2018-10-09 19:25:38 -0400

    Add files via upload

---
+++ 04CarColors/main.js
@@ -0,0 +1,33 @@
+
+   $(document).ready(function(){
+
+      $('#button0').hide();
+      $('#sketchholder1').hide();
+
+      $('body').on('click', function(){
+
+          $('body').on('click', function(){
+
+                $('#button0').show(); 
+
+                $('#button0').on('click', function(){
+
+                $('.highway').remove();
+                $('#button0').remove();
+                $('h4').remove();
+                $('#dendogramholder').remove();
+                $('#footnote2').text("Analysis includes Personal Vehicles only: 1,767,576 cars total");
+                $('#footnote2').css("font-weight", "bold");
+                $('#sketchholder1').show();
+
+                 
+
+        });
+         
+        });
+
+        });
+
+        });
+
+  
